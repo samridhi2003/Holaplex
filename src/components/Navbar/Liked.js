4711f728355610e97294c0db4a43bd8f5a6694d9
aryan <agarwalaryan139@gmail.com>
2023-06-21 04:43:28 +0530

    fix: css

--- src/components/Navbar/Liked.js
+++ src/components/Navbar/Liked.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import { SlCheck } from "react-icons/sl";
 import { RxCrossCircled } from "react-icons/rx";
 
-
 export const Liked = () => {
-    const [isLiked , setIsliked]= useState(false);
-     
-    const likedOnChange = () => {
-         setIsliked(!isLiked)
-    }
+  const [isLiked, setIsliked] = useState(false);
+
+  const likedOnChange = () => {
+    setIsliked(!isLiked);
+  };
   return (
     <>
-     <div className='likeTwitter' checked={isLiked} onChange={likedOnChange}>
-          <div className='TwitterLiked'>
-            {isLiked? <SlCheck/> : <RxCrossCircled/>}
-          </div>
-       </div>
+      <span className="likeTwitter" checked={isLiked} onChange={likedOnChange}>
+        <span className="TwitterLiked">
+          {isLiked ? <SlCheck /> : <RxCrossCircled />}
+        </span>
+      </span>
     </>
   );
-}
+};
